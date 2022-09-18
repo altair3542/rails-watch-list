89ef302b4cf8c5a966ec08187d53c191af42a77c
santiago mesa <thiagomesa21@gmail.com>
2022-09-17 20:26:54 -0500

    esta vuelta esta juerte

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'lists#index'
+  resources :lists
+  resources :movies
+  resources :bookmarks
 end
